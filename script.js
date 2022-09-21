2c38859a061294d7bc5eeaa3be2bdc04e8cbecc4
Faiyaz Alam <faiyaz48989@gmail.com>
2022-09-21 16:06:59 -0400

    Demo pic

--- script.js
+++ script.js
@@ -8,7 +8,26 @@ function generatePassword(){
   var passwordLength = parseInt(userInput)
   console.log(userInput)
   //choose a length of at least 8 characters and no more than 128 characters
+  if(passwordlength<8 || passwordLength>128)
+  window.alert("choose a length of at least 8 characters and no more than 128 characters")
+  return
 }
+//confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+var
+var
+var
+var
+
+var lowerCaseLetters = ['a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbersZeroToNine = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var selectedSpecialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
+
+for(var i=0; i< lowerCaseLetters; i++){
+
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
